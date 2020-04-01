70ace2655fd47e4f040b402b7761e92da63ee052
EddyGHendrickx <matthijs.hendrickx@gmail.com>
2020-04-01 16:14:10 +0200

    Interfaces

--- TextAdventure/Skeleton.cs
+++ TextAdventure/Skeleton.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Reflection;
+
 namespace TextAdventure
 {
-    public class Skeleton
+    public class Skeleton : ICreature
     {
-        
+        static Random rand = new Random();
+        public int health;
+
+        public int Power
+        {
+            get => Power;
+            set => Power = value;
+        }
+        public int Health
+        {
+            get => health;
+            set => health = value;
+        }
     }
 }
