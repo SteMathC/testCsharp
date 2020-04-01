249e3a5ff842f73f3c3b6f4c917f86b8bf053ca2
Steve Keersmaekers <stevekeersma@gmail.com>
2020-04-01 15:46:03 +0200

    adding player and encounters

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -2,11 +2,45 @@
 
 namespace TextAdventure
 {
-    class Program
+    //resume 11.54
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static Player currentPlayer = new Player();
+        public static bool mainLoop = true;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+            Encounters.FirstEncounter();
+            while (mainLoop)
+            {
+                Encounters.RandomEncounter();
+            }
+            
+        }
+
+        private static void Start()
+        {
+            Console.WriteLine(("Steve's Dungeon!"));
+            Console.WriteLine("Name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("you wake up in a chilly dark dungeon. You don't know how you got here");
+            if (currentPlayer.name == "")
+            {
+                Console.WriteLine("You can't even remember your name");
+            }
+            else
+            {
+                Console.WriteLine("All you remember is your name " + currentPlayer.name);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("you feel around in the darkness, you find a door");
+            Console.WriteLine("you turn the handle and see it opens quite easily, you find a skeleton outside");
+            Console.WriteLine("its back is turned to you");
+
         }
     }
 }
